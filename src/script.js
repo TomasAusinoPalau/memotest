1b13eba2f52cd8151744a1ead97c9d50777c3007
Tomas Ausino Palau <tausinopalau@gmail.com>
2021-04-24 00:11:24 -0300

    funciona juego

--- src/script.js
+++ src/script.js
@@ -24,6 +24,7 @@ function distribuirColores () {
 
     randomColors.forEach((color, index) => {
         $cuadros[index].classList.add(color);
+        parejas.push($cuadros[index])
     })
 
 
@@ -33,7 +34,70 @@ function distribuirColores () {
 
 function manejarEventos () {
     
+    desbloquearInputUsuario();    
 
+}
+
+function desbloquearInputUsuario() {
+
+    let cuadradosElegidos = []
+
+    document.querySelectorAll(".cuadro").forEach(function ($cuadro) {
+
+
+        $cuadro.onclick = (e) => {
+            const $cuadrado = e.target;
+            console.log($cuadrado);
+
+            iluminarCuadrado($cuadrado)
+
+            cuadradosElegidos.push($cuadrado)
+
+            if (cuadradosElegidos.length === 2) {
+                if (cuadradosElegidos[0].className === cuadradosElegidos[1].className) {
+                    eliminarCuadrado(cuadradosElegidos[0]);
+                    eliminarCuadrado(cuadradosElegidos[1]);
+
+                    parejasUsuario.push(cuadradosElegidos[0]);
+                    parejasUsuario.push(cuadradosElegidos[1]);
+
+                    desbloquearInputUsuario()
+                } else {
+                    ocultarCuadrado(cuadradosElegidos[0])
+                    ocultarCuadrado(cuadradosElegidos[1])
+
+                    desbloquearInputUsuario()
+                }
+            } else {
+
+            }
+        }
+    })
+
+}
+
+function eliminarCuadrado($cuadrado) {
+    setTimeout(function() {
+        $cuadrado.parentElement.classList.add('completo');
+        $cuadrado.remove();
+        evaluarFinDeJuego();
+      }, 500);
+} 
+
+function ocultarCuadrado($cuadrado) {
+    setTimeout(function() {
+        $cuadrado.style.opacity = "0";
+    }, 500)
+}
+
+function iluminarCuadrado($cuadrado) {
+    $cuadrado.style.opacity = "1";
+}
+
+function evaluarFinDeJuego() {
+    if (parejas.length === parejasUsuario.length) {
+        console.log("ganaste");
+    }
 }
 
 nuevaPartida();
